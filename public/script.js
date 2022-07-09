ced2b20872f11f8a75e3ef74938ee25e51099845
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-07-09 17:41:21 +0800

    Quick Fix, default version 2

--- public/script.js
+++ public/script.js
@@ -8,7 +8,7 @@ var pageNumber = new URLSearchParams(window.location.search).get("page");
 var version = new URLSearchParams(window.location.search).get("v");
 
 if(!pageNumber || !Number(pageNumber)) pageNumber = 1;
-if(!version || !Number(version)) version = 1;
+if(!version || !Number(version)) version = 2;
 fetch(`/v${version}/articles?page=${pageNumber}`).then(res => res.json()).then(data => {
     if(!Array.isArray(data.articles))return;
     loading.classList.add('hidden');
@@ -33,7 +33,7 @@ fetch(`/v${version}/articles?page=${pageNumber}`).then(res => res.json()).then(d
     currentPage.classList.add("page");
     currentPage.textContent = `Page ${pageNumber} of ${totalPages}`;
     document.body.append(currentPage);
-}, 500);
+}, 300);
 })
 document.getElementById("prev").onclick = () => {
     if(pageNumber == 1)return;
